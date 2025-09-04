3f17aa5af48f7ae996169145dc27d541f3176fba
ARTMARVL <vladyslav.anishchenko@gmail.com>
2025-09-04 21:45:48 +0200

    1

---
+++ src/utils/createFakeContact.js
@@ -0,0 +1,9 @@
+import { faker } from "@faker-js/faker"; 
+
+export const createFakeContanct = () => ({
+    id: faker.string.uuid(),
+    name: faker.person.fullName(),
+   phone: faker.phone.number(),
+    email: faker.internet.email(),
+   job: faker.person.jobTitle(),
+});
